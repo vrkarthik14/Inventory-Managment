b4ecf68e166ece3c7ab77fb989f3a6222731e431
Guardian-of-code <vrkarthik14@gmail.com>
2018-06-19 11:57:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/codex_pc/inventoryapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.codex_pc.inventoryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

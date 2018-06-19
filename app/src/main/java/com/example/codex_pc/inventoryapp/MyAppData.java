c25c59071f3cfdfeafe9bf833491fedf4b657573
Guardian-of-code <vrkarthik14@gmail.com>
2018-06-19 16:17:58 +0530

    view template

--- app/src/main/java/com/example/codex_pc/inventoryapp/MyAppData.java
+++ app/src/main/java/com/example/codex_pc/inventoryapp/MyAppData.java
@@ -2,10 +2,17 @@ package com.example.codex_pc.inventoryapp;
 
 import android.app.Application;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class MyAppData extends Application{
 
+    DatabaseReference product_databaseReference,supplier_databaseReference,transaction_databaseReference,customer_databaseReference;
     ArrayList<Product> products;
     ArrayList<Supplier> suppliers;
     ArrayList<Customer> customers;
@@ -21,6 +28,138 @@ public class MyAppData extends Application{
         suppliers = new ArrayList<>();
         customers = new ArrayList<>();
         transactions = new ArrayList<>();
+        product_databaseReference = FirebaseDatabase.getInstance().getReference().child("products");
+        supplier_databaseReference = FirebaseDatabase.getInstance().getReference().child("suppliers");
+        transaction_databaseReference = FirebaseDatabase.getInstance().getReference().child("transactions");
+        customer_databaseReference = FirebaseDatabase.getInstance().getReference().child("customers");
+
+        product_databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                products.add((Product) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        customer_databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                customers.add((Customer) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        supplier_databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                suppliers.add((Supplier) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        transaction_databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                transactions.add((Transaction) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
+
+
+    public void pushProducts(Product product){
+        product_databaseReference.push().setValue(product);
+    }
+
+    public void pushSuplliers(Supplier supplier){
+        supplier_databaseReference.push().setValue(supplier);
+    }
+
+    public void pushCustomers(Customer customer){
+        customer_databaseReference.push().setValue(customer);
+    }
+    public void pushTransaction(Transaction transaction){
+        transaction_databaseReference.push().setValue(transaction);
+    }
+
+
+
+
 }

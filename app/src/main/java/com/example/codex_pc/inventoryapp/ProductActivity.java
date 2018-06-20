95c5e1c71ac4e727856eea086d1a727176b199e2
Guardian-of-code <vrkarthik14@gmail.com>
2018-06-20 16:59:30 +0530

    RefreshLayout Added

--- app/src/main/java/com/example/codex_pc/inventoryapp/ProductActivity.java
+++ app/src/main/java/com/example/codex_pc/inventoryapp/ProductActivity.java
@@ -3,6 +3,7 @@ package com.example.codex_pc.inventoryapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
@@ -11,15 +12,17 @@ import java.util.ArrayList;
 
 public class ProductActivity extends AppCompatActivity {
 
+    ArrayList<Product> products;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
 
+        final SwipeRefreshLayout refreshLayout = findViewById(R.id.swiperefresh);
 
-        ArrayList<Product> products = ((MyAppData)this.getApplication()).getProducts();
+         products = ((MyAppData)this.getApplication()).getProducts();
         //Log.i("Check",products.get(0).getName());
-        ProductAdapter productAdapter = new ProductAdapter(this,products);
+        final ProductAdapter productAdapter = new ProductAdapter(this,products);
         ListView product_list_view = findViewById(R.id.product_list);
         product_list_view.setAdapter(productAdapter);
         FloatingActionButton addProducts = findViewById(R.id.add_products);
@@ -27,6 +30,15 @@ public class ProductActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(ProductActivity.this, FormActivity.class));
+            }
+        });
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                products = ((MyAppData)getApplication()).getProducts();
+                productAdapter.notifyDataSetChanged();
+                refreshLayout.setRefreshing(false);
             }
         });
 
--- app/src/main/java/com/example/codex_pc/inventoryapp/SupplierActivity.java
+++ app/src/main/java/com/example/codex_pc/inventoryapp/SupplierActivity.java
@@ -3,6 +3,7 @@ package com.example.codex_pc.inventoryapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
@@ -11,14 +12,18 @@ import java.util.ArrayList;
 
 public class SupplierActivity extends AppCompatActivity {
 
+    ArrayList<Supplier> suppliers;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supplier);
 
-        ArrayList<Supplier> suppliers= ((MyAppData)this.getApplication()).getSuppliers();
+        final SwipeRefreshLayout refreshLayout2 = findViewById(R.id.swiperefresh2);
+
+        suppliers = ((MyAppData)this.getApplication()).getSuppliers();
         //Log.i("Check",products.get(0).getName());
-        SupplierAdapter supplierAdapter = new SupplierAdapter(this,suppliers);
+        final SupplierAdapter supplierAdapter = new SupplierAdapter(this,suppliers);
         ListView product_list_view = findViewById(R.id.supplier_list);
         product_list_view.setAdapter(supplierAdapter);
 
@@ -29,6 +34,16 @@ public class SupplierActivity extends AppCompatActivity {
                 startActivity(new Intent(SupplierActivity.this, FormActivity.class));
             }
         });
+
+        refreshLayout2.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                suppliers= ((MyAppData)getApplication()).getSuppliers();
+                supplierAdapter.notifyDataSetChanged();
+                refreshLayout2.setRefreshing(false);
+            }
+        });
+
 
 
 
